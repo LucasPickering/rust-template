c48a305103ec638d2fb568d52448457dba2ad87e
Lucas Pickering <lucas@lucaspickering.me>
2026-05-02 11:18:30 -0400

    Update linting rules

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,6 +1,3 @@
-#![forbid(unsafe_code)]
-#![deny(clippy::all)]
-
 pub fn hello() {
     todo!()
 }
--- crate/src/main.rs
+++ crate/src/main.rs
@@ -1,6 +1,3 @@
-#![forbid(unsafe_code)]
-#![deny(clippy::all)]
-
 fn main() {
     println!("Hello world!");
 }
